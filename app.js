139051aa729a01c72cd1474e872cd313ca549349
Tyler McCracken <tylerdean.w@gmail.com>
2020-02-21 16:17:53 -0500

    attendees: get post put and delete

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ app.get('/api/meetings', (req, res) => {
         })
       });
 });
-
 app.put('/api/meeting/', (req, res) => {
     const meeting = req.body.meeting;
     const authToken = req.body.authToken;
@@ -78,8 +77,6 @@ app.put('/api/meeting/', (req, res) => {
 app.post('/api/meeting/', (req, res) => {
     const meeting = req.body.meeting;
     const authToken = req.body.authToken;
-    console.log('MEETING: ', meeting);
-    console.log('AUTHTOKEN: ', authToken);
     let consultometer_db = require('nano')({
         url: 'http://64.225.122.227:5984/consultometer',
         cookie: 'AuthSession='+authToken,
@@ -92,7 +89,77 @@ app.post('/api/meeting/', (req, res) => {
         })
     });
 });
-
+app.get('/api/attendees', (req, res) => {
+    console.log('req.body: ', req.query);
+    let auth = req.query.token;
+    let consultometer_db = require('nano')({
+        url: 'http://64.225.122.227:5984/consultometer',
+        cookie: 'AuthSession='+auth,
+    })
+    consultometer_db.view('attendees', 'attendee-view', function (err, body, headers) {
+        if (err) {
+          res.json({
+              err
+          });
+        }
+      
+        res.json({
+            body
+        })
+      });
+});
+app.post('/api/attendee/', (req, res) => {
+    const attendee = req.body.attendee;
+    const authToken = req.body.authToken;
+    let consultometer_db = require('nano')({
+        url: 'http://64.225.122.227:5984/consultometer',
+        cookie: 'AuthSession='+authToken,
+    })
+    consultometer_db.insert(attendee, '').then((body) => {
+        console.log('BODY: ', body);
+        //const token = headers['set-cookie'][0].split('=')[1].split('; ')[0];
+        res.json({
+            body
+        })
+    });
+});
+app.put('/api/attendee/', (req, res) => {
+    const attendee = req.body.attendee;
+    const authToken = req.body.authToken;
+    console.log('MEETING: ', attendee);
+    console.log('authToken: ', authToken);
+    let consultometer_db = require('nano')({
+        url: 'http://64.225.122.227:5984/consultometer',
+        cookie: 'AuthSession='+authToken,
+    })
+    consultometer_db.insert(attendee.value, attendee.value).then((body) => {
+        console.log('BODY: ', body);
+        //const token = headers['set-cookie'][0].split('=')[1].split('; ')[0];
+        res.json({
+            body
+        })
+    }); 
+});
+app.delete('/api/attendee/', (req, res) => {
+    console.log(req.body);
+    const id = req.body.params.id;
+    const rev = req.body.params.rev;
+    const authToken = req.body.params.authToken;
+    console.log('ID: ', id);
+    console.log('rev: ', rev);
+    console.log('AUTHTOKEN: ', authToken);
+    let consultometer_db = require('nano')({
+        url: 'http://64.225.122.227:5984/consultometer',
+        cookie: 'AuthSession='+authToken,
+    })
+    consultometer_db.destroy(id, rev).then((body) => {
+        console.log('BODY: ', body);
+        //const token = headers['set-cookie'][0].split('=')[1].split('; ')[0];
+        res.json({
+            body
+        })
+    })
+});
 const PORT = 5000;
 
 app.listen(PORT, () => {
